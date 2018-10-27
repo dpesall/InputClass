7338108f4ee2b34f54a8090de49e061990f4fc17
Drew Pesall <pesallandrew@gmail.com>
2018-10-27 14:27:33 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		System.out.print("Please enter a number between 1 and 100: ");
+		double num = Input.getDouble();
+		System.out.println(num);
+		double num1 = 1.11111111111111111111111111111111;
+		System.out.println(num1);
+	}
+}
